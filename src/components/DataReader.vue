c180d1623e15a8d2495a74996a29cc74418bb446
Nick Moore <nickmoore_1@live.com>
2019-06-21 21:57:06 -0400

    Beginning the styling work

---
+++ src/components/DataReader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <div
+        v-for="(show, index) in ldr_data.ldr.shows"
+        :key="index"
+        class="showContainer"
+      >
+        <h2>{{ show.title }}</h2>
+        <img :src="show.imageUrl" :alt="show.title + ' poster image'">
+      </div>
+  </div>
+</template>
+
+<script>
+import LDR from '@/docs/ldr-rank.json';
+
+export default {
+  name: 'DataReader',
+  data() {
+    return {
+      ldr_data: LDR,
+    };
+  },
+};
+</script>
+
+<style>
+  .showContainer {
+    background-color: #e6e6e6;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <data-reader></data-reader>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import DataReader from '@/components/DataReader.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    DataReader,
   },
 };
 </script>
